54d047d4eb4527b9cd498b3df46318e567e90039
Barry Mays <barrykmays@gmail.com>
2024-05-13 15:59:59 +0200

    add subtraction game

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,6 +32,11 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+
     } else {
         alert(`unknown game type: ${gameType}`)
         throw `unknown game type: ${gameType}. Aborting!`
@@ -67,6 +72,10 @@ function calculateCorrectAnswer() {
 
     if (operator === '+') {
         return [operand1 + operand2, "addition"];
+    } else if (operator === 'x') { 
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === '-') {
+        return [operand1 - operand2, "subtract"];
     } else {
         alert(`unknown`);
         throw `unimplemented operator $(operator)`
@@ -92,16 +101,21 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1
-    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = '+'
 
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = '-'
 }
 
-function displayMultiplyQuestion() {
-
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = 'x'
 }
